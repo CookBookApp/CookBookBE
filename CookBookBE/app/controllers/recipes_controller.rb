30705f92e68d068a28090b06a530a2428ac4da29
github username <github email address>
2020-01-21 14:50:01 -0500

    fixed the recipe create method

--- CookBookBE/app/controllers/recipes_controller.rb
+++ CookBookBE/app/controllers/recipes_controller.rb
@@ -10,11 +10,33 @@ class RecipesController < ApplicationController
     end
 
     def create
-        
+        puts params
+        @recipe = Recipe.create(recipe_params)
+        puts @recipe
+        params['keywords'].each do |keyw|
+            @keyword = Keyword.find { |key| key.keyword == keyw}
+            RecipeKeyword.create(recipe: @recipe, keyword: @keyword)
+        end
+
+        params['ingredients'].each do |ingredient| 
+            Ingredient.create(ingredient: ingredient, recipe: @recipe)
+        end
+
+        params['steps'].each do |step|
+            Step.create(content: step, recipe: @recipe)
+        end
+
+        render json: @recipe
     end
 
     def destroy
         @recipe = Recipe.find(params[:id])
         @recipe.destroy
     end
+end
+
+private
+
+def recipe_params
+    params.permit(:description,:image,:prep_time,:cook_time,:total_time,:user_id)
 end
